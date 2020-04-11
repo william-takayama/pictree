f824d4a6b146f26e5fd4f50337c9b850e2e68c74
William <william-sobreira@hotmail.com>
2020-04-11 12:52:43 -0300

    implementando upload de fotos

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -7,7 +7,7 @@ const API = 'http://localhost:3000';
 @Injectable({ providedIn : 'root' }) // Any component can hava acces to http
 export class PhotoService {
 
-    constructor(private http: HttpClient){ // Private - No one is allowed to acces http, besides the class
+    constructor(private http: HttpClient) { // Private - No one is allowed to acces http, besides the class
 
     }
     // Must return just the list
@@ -16,11 +16,20 @@ export class PhotoService {
             .get<Photo[]>(`${API}/${userName}/photos`) // Assures to return a OBJECT ARRAY 
     }
     
-    listFromUserPaginated(userName: string, page: number){
+    listFromUserPaginated(userName: string, page: number) {
         const params = new HttpParams()
             .append('page', page.toString());
         return this.http   
             .get<Photo[]>(`${API}/${userName}/photos`, { params })
+    }
+
+    upload(description: string, allowComments: boolean, file: File) {
+        // I'm not sending a JSON, must send FormData
+        const formData = new FormData();
+        formData.append('description', description);
+        formData.append('allowComments', allowComments ? 'true' : 'false'); // Must be converted to string
+        formData.append('imageFile', file);
+        return this.http.post(API + '/photos/upload', formData);
     }
 
 }
